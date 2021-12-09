3f1783c80c40fcd3d68e5f58943de3157447b4d1
cyberianwilderness <90196442+cyberianwilderness@users.noreply.github.com>
2021-12-09 22:37:34 +0000

    Delete inc directory

--- inc/customizer/options/homepage.php
+++
@@ -1,8 +0,0 @@
-<?php
-$wpc->add_control( new Olsen_Light_Customize_Slick_Control( $wpc, 'home_slider', array(
-	'section'     => 'homepage',
-	'label'       => __( 'Home Slider', 'olsen-light' ),
-	'description' => __( 'Fine-tune the homepage slider.', 'olsen-light' ),
-), array(
-	'taxonomy' => 'category',
-) ) );
